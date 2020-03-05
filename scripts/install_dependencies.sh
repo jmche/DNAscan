73e9d694cb7848644ef5f787636f793fa8411d83
James <jmche@outlook.com>
2020-03-05 23:42:57 +0900

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -154,3 +154,9 @@ echo "###########################################IMPORTANT######################
 echo "Hisat2-build and bwa-index are still creating their indexes. Please wait untill they complete their task."
 echo "You can check whether or not they are still running using the 'top' command"
 echo "##########################################################################################################"
+
+echo "download files and waiting for it"
+wait
+echo "All is done"
+
+
